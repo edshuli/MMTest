1cfadc51b456a4221067d92fb26d0480886635d4
edshuli <edna_shuli@hotmail.gr>
2020-10-19 00:59:04 +0200

    finished logo

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+var slideIndex = 1;
+showImages(slideIndex);
+
+// Next/previous controls
+function slideImage(n) {
+    showImages(slideIndex += n);
+}
+
+function showImages(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
